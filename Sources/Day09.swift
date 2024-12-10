9a565f560a42534e1e76d452380d5b93212a43a7
Pavel Kroupa <kroupa.pavel@icloud.com>
2024-12-10 18:05:52 +0100

    Steal Day 9 solution

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -5,236 +5,190 @@
 //  Created by Pavel Kroupa on 09.12.2024.
 //
 
-import Algorithms
+// https://github.com/gereons/AoC2024/blob/main/Sources/Day09/Day09.swift
+
+extension Collection {
+  subscript(safe index: Index) -> Element? {
+    indices.contains(index) ? self[index] : nil
+  }
+}
 
 struct Day09: AdventDay {
   var data: String
 
-  enum FormatType: Equatable, CustomStringConvertible {
-    case filePart(id: Int, blockSize: Int)
-    case empty
+  let map: [Int]
 
-    var description: String {
-      switch self {
-      case .empty:
-        return "."
-      case let .filePart(id, _):
-        return String(id)
-      }
-    }
-  }
-
-  func parseInput() -> [FormatType] {
-    var result = [FormatType]()
-
-    var isEmptySpace = false
-
-    var currentID = 0
-
-    for char in data {
-      guard let number = char.wholeNumberValue else { continue }
-
-      if isEmptySpace {
-        result.append(contentsOf: Array(repeating: .empty, count: number))
-      }
-
-      else {
-        result.append(contentsOf: Array(repeating: .filePart(id: currentID, blockSize: number), count: number))
-        currentID += 1
-      }
-
-      isEmptySpace.toggle()
-    }
-
-    return result
-  }
-
-  func moveFileBlocks(_ diskMap: [FormatType]) -> [FormatType] {
-    var diskMap = diskMap
-
-    var tail = diskMap.count - 1
-
-    diskLoop: for head in 0 ..< diskMap.count {
-      if diskMap[head] == .empty {
-        while diskMap[tail] == .empty {
-          if tail <= head { break diskLoop }
-          tail -= 1
-        }
-
-        diskMap.swapAt(head, tail)
-      }
-    }
-
-    return diskMap
-  }
-
-  func moveWholeBlockSSs(_ diskMap: [FormatType]) -> [FormatType] {
-    var diskMap = diskMap
-
-    var lastFileId: Int! = nil
-    if case let .filePart(id, _) = diskMap.last(where: { if case .filePart = $0 { return true } else { return false } }) {
-      lastFileId = id
-    }
-
-    var tail = diskMap.count - 1
-    var currentEmptySpaceCount = 0
-    var head = 0
-
-    while lastFileId >= 0 {
-      var movedBlock = false
-
-      while head < diskMap.count {
-        if diskMap[head] == .empty {
-          currentEmptySpaceCount += 1
-          head += 1
-          continue
-        }
-
-        if currentEmptySpaceCount != 0 {
-          head -= currentEmptySpaceCount
-          tail = diskMap.count - 1
-
-          // Try to find a matching block from the end
-          while head < tail {
-            if case let .filePart(id, blockSize) = diskMap[tail] {
-              if lastFileId == id {
-                if blockSize <= currentEmptySpaceCount {
-                  // Move the block
-                  for _ in 0 ..< blockSize {
-                    diskMap.swapAt(head, tail)
-                    head += 1
-                    tail -= 1
-                  }
-                  movedBlock = true
-//                                  print(diskMap.prettyPrinted)
-                  break
-                }
-              }
-              tail -= 1
-            } else {
-              tail -= 1
-            }
-          }
-
-          if !movedBlock {
-            // If we couldn't move any block, skip this empty space
-            head += currentEmptySpaceCount
-          }
-          currentEmptySpaceCount = 0
-        }
-
-        head += 1
-      }
-
-      // Only move to next file ID if we couldn't move any more blocks of current ID
-      if !movedBlock {
-        lastFileId -= 1
-      }
-
-      // Reset for next iteration
-      head = 0
-      currentEmptySpaceCount = 0
-      tail = diskMap.count - 1
-    }
-
-    return diskMap
-  }
-
-  func moveWholeBlocks(_ diskMap: [FormatType]) -> [FormatType] {
-    var diskMap = diskMap
-
-    var currentFileID: Int! = nil
-    if case let .filePart(id, _) = diskMap.last(where: { if case .filePart = $0 { return true } else { return false } }) {
-      currentFileID = id
-    }
-
-    var tail = diskMap.count - 1
-
-    var currentEmptySpaceCount = 0
-
-    var head = 0
-
-    while currentFileID > 0 {
-      if diskMap[head] == .empty {
-        currentEmptySpaceCount += 1
-        head += 1
-        continue
-      }
-
-      if currentEmptySpaceCount != 0 {
-        head -= currentEmptySpaceCount
-
-        while true {
-          guard head < tail else {
-            currentFileID -= 1
-            head = 0
-            currentEmptySpaceCount = 0
-            tail = diskMap.count - 1
-            break
-          }
-          if case let .filePart(id, blockSize) = diskMap[tail] {
-            guard currentFileID == id else {
-              tail -= 1
-              continue
-            }
-            currentFileID -= 1
-
-            if blockSize <= currentEmptySpaceCount {
-              for _ in 0 ..< blockSize {
-                diskMap.swapAt(head, tail)
-                head += 1
-                tail -= 1
-              }
-
-              break
-
-            } else {
-              tail -= blockSize
-            }
-
-          } else {
-            tail -= 1
-          }
-        }
-        currentEmptySpaceCount = 0
-      }
-      head += 1
-    }
-
-    return diskMap
-  }
-
-  func calculateChecksum(_ diskMap: [FormatType]) -> Int {
-    var checksum = 0
-
-    for index in 0 ..< diskMap.count {
-      if case let .filePart(id, _) = diskMap[index] {
-        checksum += (index * id)
-      }
-    }
-
-    return checksum
+  init(data: String) {
+    self.data = data
+    map = data.filter { !$0.isWhitespace }.map { Int(String($0))! }
   }
 
   func part1() -> Int {
-    let diskMap = parseInput()
-    let newDiskMap = moveFileBlocks(diskMap)
+    return 0
+//    var blocks = [Int]()
+//    var fileId = 0
+//    for (index, digit) in map.enumerated() {
+//      if index.isMultiple(of: 2) {
+//        // a file
+//        blocks.append(contentsOf: [Int](repeating: fileId, count: digit))
+//        fileId += 1
+//      } else {
+//        // free space
+//        blocks.append(contentsOf: [Int](repeating: -1, count: digit))
+//      }
+//    }
+//
+//    var firstFree = -1
+//    while let firstFreeIndex = firstFreeIndex(after: firstFree, in: blocks) {
+//      var last: Int
+//      repeat {
+//        last = blocks.removeLast()
+//      } while last == -1
+//
+//      blocks[firstFreeIndex] = last
+//      firstFree = firstFreeIndex
+//    }
+//
+//    var checksum = 0
+//    for (index, id) in blocks.enumerated() {
+//      checksum += index * id
+//    }
+//    return checksum
+  }
 
-    return calculateChecksum(newDiskMap)
+  enum Block {
+    case file(id: Int, size: Int)
+    case free(size: Int)
+
+    var isFree: Bool {
+      switch self {
+      case .file: false
+      case .free: true
+      }
+    }
+
+    func isFree(minSize: Int) -> Bool {
+      switch self {
+      case .file: false
+      case let .free(size): size >= minSize
+      }
+    }
+
+    func isFile(id: Int) -> Bool {
+      switch self {
+      case let .file(fileId, _): fileId == id
+      case .free: false
+      }
+    }
+
+    var size: Int {
+      switch self {
+      case let .file(_, size): size
+      case let .free(size): size
+      }
+    }
   }
 
   func part2() -> Int {
-    let diskMap = parseInput()
+    var blocks = [Block]()
+    var fileSizes = [Int: Int]()
+    var fileId = 0
+    for (index, digit) in map.enumerated() {
+      if index.isMultiple(of: 2) {
+        // a file
+        blocks.append(.file(id: fileId, size: digit))
+        fileSizes[fileId] = digit
+        fileId += 1
+      } else {
+        // free space
+        blocks.append(.free(size: digit))
+      }
+    }
 
-    let newDiskMap = moveWholeBlocks(diskMap)
-//    print(newDiskMap.prettyPrinted)
+    var newBlocks = blocks
+    for moveId in (0 ..< fileId).reversed() {
+      let fileSize = fileSizes[moveId]!
+      let fileIndex = newBlocks.firstIndex(where: { $0.isFile(id: moveId) })!
 
-    return calculateChecksum(newDiskMap)
+      guard let freeIndex = firstFreeIndex(before: fileIndex, minSize: fileSize, in: newBlocks) else { continue }
+
+      let freeBlock = newBlocks[freeIndex]
+      let freeSize = freeBlock.size
+      newBlocks[fileIndex] = .free(size: fileSize)
+      newBlocks[freeIndex] = .file(id: moveId, size: fileSize)
+      if fileSize == freeSize {
+        mergeFree(&newBlocks, at: fileIndex)
+      } else {
+        newBlocks.insert(.free(size: freeSize - fileSize), at: freeIndex + 1)
+        mergeFree(&newBlocks, at: fileIndex + 1)
+      }
+    }
+
+    var checksum = 0
+    for (index, id) in expandBlocks(newBlocks).enumerated() {
+      if id != -1 {
+        checksum += index * id
+      }
+    }
+    return checksum
   }
-}
 
-extension Array where Element == Day09.FormatType {
-  var prettyPrinted: String {
-    map { $0.description }.joined()
+  func expandBlocks(_ blocks: [Block]) -> [Int] {
+    var result = [Int]()
+    for b in blocks {
+      switch b {
+      case let .file(id, size):
+        let arr = [Int](repeating: id, count: size)
+        result.append(contentsOf: arr)
+
+      case let .free(size):
+        let arr = [Int](repeating: -1, count: size)
+        result.append(contentsOf: arr)
+      }
+    }
+    return result
+  }
+
+  private func firstFreeIndex(after first: Int, in array: [Int]) -> Int? {
+    var index = first + 1
+    while index < array.count {
+      if array[index] == -1 { return index }
+      index += 1
+    }
+    return nil
+  }
+
+  private func firstFreeIndex(before end: Int, minSize: Int, in array: [Block]) -> Int? {
+    for index in 0 ..< end {
+      switch array[index] {
+      case .file: continue
+      case let .free(size): if size >= minSize { return index }
+      }
+    }
+    return nil
+  }
+
+  private func mergeFree(_ blocks: inout [Block], at index: Int) {
+    let b1 = blocks[safe: index - 1]
+    let b2 = blocks[index]
+    let b3 = blocks[safe: index + 1]
+
+    switch (b1, b2, b3) {
+    case let (.free(s1), .free(s2), .free(s3)):
+      blocks[index - 1] = .free(size: s1 + s2 + s3)
+      blocks.remove(at: index + 1)
+      blocks.remove(at: index)
+
+    case let (.free(s1), .free(s2), _):
+      blocks[index - 1] = .free(size: s1 + s2)
+      blocks.remove(at: index)
+
+    case let (_, .free(s1), .free(s2)):
+      blocks[index] = .free(size: s1 + s2)
+      blocks.remove(at: index + 1)
+
+    default:
+      break
+    }
   }
 }
